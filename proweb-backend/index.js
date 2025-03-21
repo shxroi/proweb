ae4e8ce914507b80034b6d0366bbaaacc71be5ef
shxroi <rakhaboy2590@gmail.com>
2025-03-21 22:38:24 +0800

    Adding Edit and Delete Fuction

--- proweb-backend/index.js
+++ proweb-backend/index.js
@@ -43,6 +43,31 @@ app.get('/produk', async (req, res) => {
 }
 });
 
+app.put('/produk/:id', async (req, res) => {
+  const { id } = req.params;
+  const { nama, harga } = req.body;
+  try {
+    const updateProduk = await pool.query(
+      'UPDATE produk SET nama = $1, harga = $2 WHERE id = $3 RETURNING *',
+      [nama, harga, id]
+    );
+    res.json(updateProduk.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
+ });
+ app.delete('/produk/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    await pool.query('DELETE FROM produk WHERE id = $1', [id]);
+    res.json({ message: 'Produk dihapus' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
+ });
+
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
 });
